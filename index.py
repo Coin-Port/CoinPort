355c19ae1e3b30266ad42b14ea5c94e97fdaa1ce
jd <robtweisz@gmail.com>
2021-04-23 22:05:37 -0700

    Add gas prices

--- index.py
+++ index.py
@@ -1,30 +1,39 @@
+import requests
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
-
-app=Flask(__name__)
-app.config['SECRET_KEY'] = '98073e958b9c236b3fa0d714c68a2e6c'
+app = Flask(__name__)
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    gas = requests.get(
+        'https://api.zapper.fi/v1/gas-price?network=ethereum&api_key=96e0cc51-a62e-42ca-acee-910ea7d2a241')
+    standard = gas.json()['standard']
+    fast = gas.json()['fast']
+    instant = gas.json()['instant']
+    return render_template('index.html', standard=int(standard), fast=int(fast), instant=int(instant))
+
 
 @app.route('/index.html')
 def home():
     return render_template('index.html')
 
+
 @app.route('/login.html')
 def login():
     return render_template('login.html')
+
 
 @app.route('/register.html')
 def register():
     return render_template('register.html')
 
+
 @app.route('/forgot-password.html')
 def forgotPassword():
     return render_template('forgot-password.html')
+
 
 @app.route('/404.html')
 def notFound():
@@ -32,4 +41,4 @@ def notFound():
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
